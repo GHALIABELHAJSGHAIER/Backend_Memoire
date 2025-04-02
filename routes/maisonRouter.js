22079e00dad9c8cc7440666a47e8229e51edaa6e
GHALIABELHAJSGHAIER <ghalia.bhs20020130@gmail.com>
2025-04-02 23:14:22 +0200

    maison

--- routes/maisonRouter.js
+++ routes/maisonRouter.js
@@ -8,10 +8,11 @@ router.post('/addMaison', maisonController.addMaison);
 
 router.post('/addMaisonForClient', maisonController.addMaisonForClient); // Ajouter une maison pour un client
 router.get('/getMaisonsByClientId/:clientId', maisonController.getMaisonsByClientId); // Obtenir toutes les maisons d'un client
+router.delete('/deleteMaisonById/:id', maisonController.deleteMaisonById); 
 router.get('/getAllMaison', maisonController.getAllMaison); 
 
 router.get('/getMaisonById/:id', maisonController.getMaisonById); 
-router.delete('/deleteMaisonById/:id', maisonController.deleteMaisonById); 
+
 router.put('/updateMaison/:id', maisonController.updateMaison); 
 router.put('/affect', maisonController.affect); 
 router.put('/desaffect', maisonController.desaffect); 
